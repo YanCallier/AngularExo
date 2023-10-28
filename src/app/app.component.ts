88f55ac672663cb5f8cb6f35e99b5ef6fb873c87
Yan Callier <yan.callier@gmail.com>
2023-10-28 12:42:24 +0200

    micro ping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myTest';
+
+  ping(): void {
+    axios({
+      method: 'OPTIONS',
+      url: 'http://localhost:3000/',
+    }).then(function (response) {
+      console.log('ping', response);
+    });
+  }
 }
